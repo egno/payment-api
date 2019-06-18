20235103ef3cbc8a555246ad995fe4edbcb13c00
Alexander Shelemetyev <ex.lxndr@gmail.com>
2019-06-18 09:38:40 +0300

    remove db

--- billing.py
+++ billing.py
@@ -2,14 +2,17 @@ from requests import post
 from config import CONFIG as config
 
 BILLING_API = config['BILLING_API']
+UNO_COMISSION_PERCENT = config['unoComissionPrecent'] or 0.0
 
 
 def payment(params):
     url = BILLING_API.get('URL') + BILLING_API.get('transactionPath')
+    unoComission = round((float(params.get('LMI_PAID_AMOUNT') or 0.0) * UNO_COMISSION_PERCENT / 100), 4)
     data = {
         "type": "CustomerPayment",
         "business": params.get('BUSINESS_ID'),
         "amount": params.get('LMI_PAID_AMOUNT'),
+        "unoComission": unoComission,
         "paymentInfo": dict([{param: params.get(param)} for param in params])
     }
     print(url, data)

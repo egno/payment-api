99a825fe58b173c402f596cdfaa2dd5ad46e904a
Alexander Shelemetyev <ex.lxndr@gmail.com>
2019-06-19 06:43:24 +0300

    fix variable

--- billing.py
+++ billing.py
@@ -2,18 +2,19 @@ from requests import post
 from config import CONFIG as config
 
 BILLING_API = config['BILLING_API']
-UNO_COMISSION_PERCENT = config['unoComissionPrecent'] or 0.0
+UNO_COMMISSION_PERCENT = config['unoCommissionPrecent'] or 0.0
 
 
 def payment(params):
     url = BILLING_API.get('URL') + BILLING_API.get('transactionPath')
     print(params)
-    unoComission = round((float(params.get('LMI_PAID_AMOUNT') or 0.0) * UNO_COMISSION_PERCENT / 100), 4)
+    unoCommission = round((float(params.get('LMI_PAID_AMOUNT') or 0.0) * UNO_COMISSION_PERCENT / 100), 4)
     data = {
         "type": "CustomerPayment",
+        "provider":params.get('provider'),
         "business": params.get('BUSINESS_ID'),
         "amount": params.get('LMI_PAID_AMOUNT'),
-        "unoComission": unoComission,
+        "unoCommission": unoCommission,
         "paymentInfo": dict([param, params.get(param)] for param in params)
     }
     print(url, data)

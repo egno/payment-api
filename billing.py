dde7dabe8bfc6b4538857a8bcfe01c7a7fd8b1e7
Alexander Shelemetyev <ex.lxndr@gmail.com>
2019-08-07 14:52:35 +0300

    change var names

--- billing.py
+++ billing.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-UNO_COMMISSION_PERCENT = float(os.getenv('PAYMENT_COMISSION', 0.0))
+UNO_COMMISSION_PERCENT = float(os.getenv('PAYMENT_COMISSION_PERCENT', 0.0))
 BILLING_API = {
         'URL': os.getenv('BILLING_API_URL'),
         'transactionPath': '/transaction'
